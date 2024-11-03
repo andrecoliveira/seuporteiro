9c85648bed2acd2ed0b183cacf47d6091c53a8db
André Oliveira <coliveira.andre@gmail.com>
2024-11-03 18:08:13 -0300

    chore: configurando o lighthouse CI

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'yarn build && yarn start',
+      startServerCommand: 'npm run build && npm start',
       url: ['http://localhost:3000'],
       startServerReadyPattern: 'Server is ready',
       startServerReadyTimeout: 120000,
