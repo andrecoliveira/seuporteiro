a4f5feb27d8b78e2446666bbf8ef9c8a4c794859
André Oliveira <coliveira.andre@gmail.com>
2024-11-07 15:17:28 -0300

    fix: refatorando interface dos formulários

--- app/painel/restaurante/page.tsx
+++ app/painel/restaurante/page.tsx
@@ -1,139 +1,60 @@
-import { Button, Input, Label } from '@/components/ui'
-import { SquarePen } from 'lucide-react'
+import { Button } from '@/components/ui'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { SquarePen, SquareArrowOutUpRight } from 'lucide-react'
 
 export default function RestaurantPage() {
+  const account = [
+    { label: 'Nome da loja', value: 'Andre Cuccina' },
+    { label: 'Telefone', value: '(85) 99974-9025' },
+    { label: 'Categoria', value: 'Pizza' },
+    {
+      label: 'Descrição',
+      value:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt justo est, et pretium turpis scelerisque a. Cras fermentum tortor sed tincidunt accumsan. Nunc commodo lorem a nunc rutrum auctor. Donec et commodo urna. Vivamus accumsan elit velit, dignissim varius leo placerat malesuada. Nullam ultrices pellentesque auctor. Suspendisse vestibulum auctor orci, non luctus tortor rutrum ac. Integer non pellentesque lorem, id aliquet erat. Proin egestas velit ex, vitae lobortis nisl imperdiet et. Integer et ultrices turpis, sed scelerisque magna.',
+    },
+  ]
+
+  function ListItems({ label, value }: { label: string; value: string }) {
+    return (
+      <div className="md:items-top md:items-top flex flex-col space-y-2 md:flex-row md:space-y-0">
+        <span className="text-sm text-gray-500 md:mb-0 md:basis-1/4">
+          {label}
+        </span>
+        <span className="text-sm md:basis-3/4">{value}</span>
+      </div>
+    )
+  }
+
   return (
     <div className="space-y-6">
-      <h3 className="text-3xl font-semibold text-slate-800">Restaurante</h3>
-
-      <div className="space-y-6">
-        <div className="flex items-center justify-between border-b pb-2">
-          <h2 className="text-lg font-semibold">Informações</h2>
-          <Button variant="outline">
-            Editar <SquarePen />
-          </Button>
-        </div>
-
-        <div>
-          <form className="space-y-2">
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label
-                htmlFor="instagram"
-                className="font-normal md:mb-0 md:w-48"
-              >
-                Nome da loja
-              </Label>
-              <div className="relative w-full">
-                <Input name="restaurant_name" className="pr-16" required />
-                <span className="absolute inset-y-0 right-3 flex items-center text-xs text-gray-500">
-                  15 / 40
-                </span>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label
-                htmlFor="instagram"
-                className="font-normal md:mb-0 md:w-48"
-              >
-                Telefone
-              </Label>
-              <Input name="phone" required />
-            </div>
-
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label
-                htmlFor="instagram"
-                className="font-normal md:mb-0 md:w-48"
-              >
-                Categoria
-              </Label>
-              <Input name="category" required />
-            </div>
-
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label
-                htmlFor="instagram"
-                className="font-normal md:mb-0 md:w-48"
-              >
-                Descrição
-              </Label>
-              <Input name="description" required />
-            </div>
-          </form>
-        </div>
+      <div className="flex justify-between">
+        <h3 className="text-3xl font-semibold text-slate-800">Restaurante</h3>
+        <Button>
+          Ver a loja <SquareArrowOutUpRight />
+        </Button>
       </div>
 
-      <div className="space-y-6">
-        <div className="flex items-center justify-between border-b pb-2">
-          <h2 className="text-lg font-semibold">Endereço</h2>
-          <Button variant="outline">
-            Editar <SquarePen />
-          </Button>
-        </div>
-
-        <div>
-          <form className="space-y-2">
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label htmlFor="cep" className="font-normal md:mb-0 md:w-48">
-                CEP
-              </Label>
-              <Input name="cep" required />
-            </div>
-
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label htmlFor="address" className="font-normal md:mb-0 md:w-48">
-                Endereço
-              </Label>
-              <Input name="address" required />
-            </div>
-
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label
-                htmlFor="street_number"
-                className="font-normal md:mb-0 md:w-48"
-              >
-                Número
-              </Label>
-              <Input name="street_number" required />
-            </div>
-
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label
-                htmlFor="complement"
-                className="font-normal md:mb-0 md:w-48"
-              >
-                Complemento
-              </Label>
-              <Input name="complement" placeholder="Opcional" required />
-            </div>
-
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label
-                htmlFor="neighborhood"
-                className="font-normal md:mb-0 md:w-48"
-              >
-                Bairro
-              </Label>
-              <Input name="neighborhood" required />
-            </div>
-
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label htmlFor="city" className="font-normal md:mb-0 md:w-48">
-                Cidade
-              </Label>
-              <Input name="city" required />
-            </div>
-
-            <div className="flex flex-col items-start space-y-2 md:flex-row md:items-center md:space-y-0">
-              <Label htmlFor="state" className="font-normal md:mb-0 md:w-48">
-                Estado
-              </Label>
-              <Input name="state" required />
-            </div>
-          </form>
-        </div>
-      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center justify-between">
+            Detalhes da conta
+            <Button variant="outline">
+              Editar <SquarePen />
+            </Button>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-5">
+            {account.map((item) => (
+              <ListItems
+                key={item.label}
+                label={item.label}
+                value={item.value}
+              />
+            ))}
+          </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
