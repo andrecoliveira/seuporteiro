b41f88eaabfd91503bed8abedae02a2f27ad802b
André Oliveira <coliveira.andre@gmail.com>
2024-12-26 15:53:07 -0300

    Refatorando o CreateClient

--- actions/create-tenant.ts
+++ actions/create-tenant.ts
@@ -1,7 +1,12 @@
-import { supabaseClient } from '@/lib/supabaseClient'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+)
 
 export const createTenant = async (stripeCustomerId: string) => {
-  const { data, error } = await supabaseClient
+  const { data, error } = await supabase
     .from('tenants')
     .insert([
       {
--- actions/create-user.ts
+++ actions/create-user.ts
@@ -1,10 +1,15 @@
-import { supabaseClient } from '@/lib/supabaseClient'
 import { User } from '@/types/user'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+)
 
 export const createtUser = async (
   formData: Omit<User, 'stripeCustomerId' | 'tenantId'>,
 ) => {
-  const response = await supabaseClient.from('users').insert([
+  const response = await supabase.from('users').insert([
     {
       email: formData.email,
       first_name: formData.firstName,
