88d4137cd36dd5a4b4543b051338c15b00b26981
André Oliveira <coliveira.andre@gmail.com>
2024-12-26 19:39:54 -0300

    Capturando erros

--- actions/create-user.ts
+++ actions/create-user.ts
@@ -1,15 +1,11 @@
+'use server'
+
 import { User } from '@/types/user'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  {
-    auth: {
-      autoRefreshToken: false,
-      persistSession: false,
-    },
-  },
+  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  process.env.SUPABASE_SERVICE_ROLE_KEY || '',
 )
 
 export const createtUser = async (
