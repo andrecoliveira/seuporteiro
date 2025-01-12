d2dec87d15911a532e86074ca0f89c1f49f48dcf
André Oliveira <coliveira.andre@gmail.com>
2025-01-12 11:55:40 -0300

    Efetuando correções no clerk webhook

--- actions/user.ts
+++ actions/user.ts
@@ -2,6 +2,14 @@ import { supabaseAdmin } from '@/lib/supabaseClient'
 import { User } from '@/types/user'
 
 export async function createUserByWebhook(user: User) {
-  const { error } = await supabaseAdmin.from('users').insert([user])
+  const { error } = await supabaseAdmin.from('users').insert([
+    {
+      email: user.email_addresses[0].email_address,
+      user_id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      profile_image_url: user.profile_image_url,
+    },
+  ])
   if (error) throw new Error(error.message)
 }
