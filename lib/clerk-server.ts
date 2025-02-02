10f01a60278f9581d33e0c52bfcabc1c533d7a8c
André Oliveira <coliveira.andre@gmail.com>
2025-02-02 11:59:59 -0300

    Aplicando as refatorações do SonarQube

--- lib/clerk-server.ts
+++ lib/clerk-server.ts
@@ -2,26 +2,29 @@ import { auth } from '@clerk/nextjs/server'
 import { createClient } from '@supabase/supabase-js'
 
 export function createClientServer(token: string) {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      global: {
-        // Get the custom Supabase token from Clerk
-        fetch: async (url, options = {}) => {
-          // Insert the Clerk Supabase token into the headers
-          const headers = new Headers(options?.headers)
-          headers.set('Authorization', `Bearer ${token}`)
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-          // Now call the default fetch
-          return fetch(url, {
-            ...options,
-            headers,
-          })
-        },
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing Supabase environment variables')
+  }
+
+  return createClient(supabaseUrl, supabaseAnonKey, {
+    global: {
+      // Get the custom Supabase token from Clerk
+      fetch: async (url, options = {}) => {
+        // Insert the Clerk Supabase token into the headers
+        const headers = new Headers(options?.headers)
+        headers.set('Authorization', `Bearer ${token}`)
+
+        // Now call the default fetch
+        return fetch(url, {
+          ...options,
+          headers,
+        })
       },
     },
-  )
+  })
 }
 
 export async function getAuth() {
