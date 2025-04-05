1fecd84d928b2cad5ebdbf2e90a520a1c83b148f
André Oliveira <coliveira.andre@gmail.com>
2025-04-05 13:45:59 -0300

    Ajustando Clerk webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server'
 
-import { createUserByWebhook } from '@/actions/user'
+// import { createUserByWebhook } from '@/actions/user'
 import { userAdapter } from '@/data/adapters/clerk'
+import { supabaseClient } from '@/lib/supabaseClient'
 
 import { handleError } from '@/utils/handle-errors'
 import { validateWebhook } from '@/utils/svix'
@@ -14,7 +15,17 @@ export async function POST(req: Request) {
 
     if (eventType === 'user.created') {
       const client = userAdapter(payload?.data)
-      await createUserByWebhook(payload?.data)
+      // await createUserByWebhook(payload?.data)
+      const { error, data } = await supabaseClient.from('users').insert([
+        {
+          email: payload?.data.email_addresses[0].email_address,
+          user_id: payload?.data.id,
+          first_name: payload?.data.first_name,
+          last_name: payload?.data.last_name,
+          profile_image_url: payload?.data.profile_image_url,
+        },
+      ])
+      console.log({ error, data })
       console.log(`${client.email} - User created successfully`)
       return NextResponse.json(
         { message: 'User created successfully' },
